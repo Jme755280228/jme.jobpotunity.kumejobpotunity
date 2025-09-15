baa39ddbdd2d10a2aef86bbef1edfb67dc287170
Jme755280228 <zawminhtayjme@gmail.com>
2025-09-15 13:51:05 +0630

    CommandLineRunner method @Bean is deleted.At there, new-job.html form adding jobPosting section is added.

--- src/main/java/jme/jobpotunity/kumejobpotunity/KumejobpotunityApplication.java
+++ src/main/java/jme/jobpotunity/kumejobpotunity/KumejobpotunityApplication.java
@@ -16,24 +16,4 @@ public class KumejobpotunityApplication {
 		SpringApplication.run(KumejobpotunityApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner run(CompanyRepository companyRepository, JobPostingRepository jobPostingRepository) {
-		return args -> {
-			// Company Entities တွေကို ဖန်တီးပြီး သိမ်းဆည်းခြင်း
-			Company google = new Company("Google", "Technology", "ရန်ကုန်မြို့");
-			Company facebook = new Company("Facebook", "Technology", "မန္တလေးမြို့");
-
-			companyRepository.save(google);
-			companyRepository.save(facebook);
-
-			// JobPosting Entities တွေကို ဖန်တီးပြီး သိမ်းဆည်းခြင်း
-			JobPosting seniorDev = new JobPosting("Senior Developer", "We are looking for a senior developer...", "ရန်ကုန်မြို့", "၂၀၀၀၀၀၀ ကျပ်", "Full-Time", google);
-			JobPosting hrManager = new JobPosting("HR Manager", "We need an HR manager with experience...", "မန္တလေးမြို့", "၁၅၀၀၀၀၀ ကျပ်", "Full-Time", facebook);
-			JobPosting intern = new JobPosting("Intern", "We need interns for our team...", "ရန်ကုန်မြို့", "၃၀၀၀၀၀ ကျပ်", "Part-Time", google);
-
-			jobPostingRepository.save(seniorDev);
-			jobPostingRepository.save(hrManager);
-			jobPostingRepository.save(intern);
-		};
-	}
 }
